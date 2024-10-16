7f3ab035109bfcebbc6425007a51a0bb4fd34181
dadahon <dadahon@gmail.com>
2024-10-16 10:24:11 +0500

    updated

--- app.js
+++ app.js
@@ -152,43 +152,43 @@
 
 // PRODUCTS MASSIVI (9-15 MASALALAR UCHUN)
 
-let products = [
-  {
-    id: 6,
-    name: "Smarthpone",
-    price: 12000,
-    rating: 4.5,
-    discount: 20,
-  },
-  {
-    id: 2,
-    name: "Acer",
-    price: 10000,
-    rating: 4.3,
-    discount: 10,
-  },
-  {
-    id: 1,
-    name: "Mac book",
-    price: 17000,
-    rating: 4.7,
-    discount: 40,
-  },
-  {
-    id: 4,
-    name: "HP",
-    price: 21000,
-    rating: 4.1,
-    discount: 30,
-  },
-  {
-    id: 5,
-    name: "Dell",
-    price: 35000,
-    rating: 4.9,
-    discount: 30,
-  },
-];
+// let products = [
+//   {
+//     id: 6,
+//     name: "Smarthpone",
+//     price: 12000,
+//     rating: 4.5,
+//     discount: 20,
+//   },
+//   {
+//     id: 2,
+//     name: "Acer",
+//     price: 10000,
+//     rating: 4.3,
+//     discount: 10,
+//   },
+//   {
+//     id: 1,
+//     name: "Mac book",
+//     price: 17000,
+//     rating: 4.7,
+//     discount: 40,
+//   },
+//   {
+//     id: 4,
+//     name: "HP",
+//     price: 21000,
+//     rating: 4.1,
+//     discount: 30,
+//   },
+//   {
+//     id: 5,
+//     name: "Dell",
+//     price: 35000,
+//     rating: 4.9,
+//     discount: 30,
+//   },
+// ];
 // 9-MASALA. Products massivini id, name, price, rating va discount bo'yicha sortlash; (sort)
 
 // SOLUTION
